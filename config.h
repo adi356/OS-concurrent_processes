2275d8a73fa57af2d9d369b30c4560d8e517f4df
Andrew <adi356@mail.umsl.edu>
2023-09-27 14:13:40 -0600

    added max processes to config.h

---
+++ config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define MAX_PROCESSES 20
+//#define MAX_ATTEMPTS 5
+//TODO: need to define max amount of time before termination
+#define LOG_FILE_PREFIX "logfile."
+
+#endif
