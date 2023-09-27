71925e4545a154959c90352d58827c1ae3ce969a
Andrew <adi356@mail.umsl.edu>
2023-09-27 14:28:59 -0600

    added all necessary libraries - I think

---
+++ master.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <config.h>
+
+int main(int argc, char* argv[]) {
+    
+}
