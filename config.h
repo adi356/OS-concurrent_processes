f6f0f82d68186ee0527a7c080ad801619426954e
adi356 <adi356@umsystem.edu>
2023-09-28 21:38:06 -0600

    changed master logic added DEFAULT_TIME config.h

--- config.h
+++ config.h
@@ -2,8 +2,7 @@
 #define CONFIG_H
 
 #define MAX_PROCESSES 20
-//#define MAX_ATTEMPTS 5
-//TODO: need to define max amount of time before termination
+#define DEFAULT_TIMEOUT 100
 #define LOG_FILE_PREFIX "logfile."
 
 #endif
--- master.c
+++ master.c
@@ -34,51 +34,28 @@ struct SharedData* sharedData;
 void terminationFunc(int signo) {
     printf("Received termination signal. Cleaning up processes\n");
 
-    //third expression might need to be changed to ++i instead of i++ (?)
-    for (int i = 0; i < MAX_PROCESSES; i++) {
-        if (sharedData[i].inCritical) {
-            //itoa not working properly, commenting out and using sprintf instead 
-            //FILE* logFile = fopen(strcat(LOG_FILE_PREFIX, itoa(i + 1)), "a");
-            char fileName[100];
-            sprintf(fileName, "%s%d", LOG_FILE_PREFIX, i + 1);
-            FILE* logFile = fopen(fileName, "a");
-
-            time_t rawTime;
-            time(&rawTime);
-            fprintf(logFile, "Termination time: %s", ctime(&rawTime));
-            fclose(logFile);
-            printf("Terminating child process %d\n", i + 1);
-            kill(i + 1, SIGTERM);
-        }
-    }
-
-    //Block deallocates shared memory and display updated info
-    system("ipcs -m");
+    //deallocate shared memory
     shmdt(sharedData);
-    //should create shared memory segment, get identifier and then remove it
-    shmctl(shmget(IPC_PRIVATE, sizeof(struct SharedData) * MAX_PROCESSES, IPC_CREAT | 0666), IPC_RMID, NULL);
-    system("ipcs -m");
+    shmctl(shmget(ftok("shared_memory_key", 1), sizeof(struct SharedData) * MAX_PROCESSES, IPC_CREAT | 0666), IPC_RMID, NULL);
+    
+    //print termination time
+    time_t t;
+    time(&t);
+    printf("Terminated at: %s", ctime(&t));
 
     exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s -t <timeout_seconds> <number_of_processes>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+  
     //signal handlers
     signal(SIGTERM, terminationFunc);
     signal(SIGINT, terminationFunc);
 
-    //handles command line input, makes sure number of processes generated is less than MAX_PROCESS
-    int timeout = atoi(argv[2]);
-    int numProcesses = atoi(argv[3]);
+    int timeout = (argc > 2) ? atoi(argv[2]) : DEFAULT_TIMEOUT;
+    int numProcesses = (argc > 3) ? atoi(argv[3]) : MAX_PROCESSES;
     numProcesses = (numProcesses > MAX_PROCESSES) ? MAX_PROCESSES : numProcesses;
 
-    //code edited from stackoverflow
-    //setup shared memory segment and obtain pointer to it
-    //key is used for other processes that need to attach to the same shared memory segment
     key_t key = ftok("shared_memory_key", 1);
     int shmid = shmget(key, sizeof(struct SharedData) * MAX_PROCESSES, IPC_CREAT | 0666);
     sharedData = (struct SharedData*)shmat(shmid, NULL, 0);
@@ -93,17 +70,36 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < numProcesses; ++i) {
         pid_t pid = fork();
         if (pid == 0) {
-           //execlp("./slave", "slave", itoa(i + 1), NULL);
-           char processNumberStr[10];
-           sprintf(processNumberStr, "%d", i + 1);
-           execlp("./slave", "slave", processNumberStr, NULL);
-           //need to be more descriptive
+           execlp("./slave", "slave", argv[1], NULL);
+           //TODO: write more descriptive error
            perror("execlp");
-           exit(EXIT_FAILURE); 
+           exit(EXIT_FAILURE);
         } else if (pid < 0) {
-           //need to describe perror better
-           perror("fork");
-           exit(EXIT_FAILURE); 
+            //TODO: write more descriptive error
+            perror("fork");
+            exit(EXIT_FAILURE);
         }
     }
+
+    sleep(timeout);
+
+    //terminate child processes
+    for (int i = 0; i < numProcesses; ++i) {
+        kill(0, SIGTERM);
+    }
+
+    //wait for child processes to terminate
+    int status;
+    while (wait(&status) > 0);
+
+    //deallocate shared memory
+    shmdt(sharedData);
+    shmctl(shmget(key, sizeof(struct SharedData) * MAX_PROCESSES, IPC_CREAT | 0666), IPC_RMID, NULL);
+
+    //print time
+    time_t t;
+    time(&t);
+    printf("Terminated at: %s", ctime(&t));
+
+    return 0;
 }
