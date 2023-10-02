53aa2baaf5fa58843a400ce390bf0ec07b2cac76
adi356 <adi356@umsystem.edu>
2023-10-02 12:55:44 -0600

    added logic for logFile and write to cstest

--- slave.c
+++ slave.c
@@ -5,6 +5,10 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <time.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
 
 struct SharedData {
     int turn;
@@ -13,26 +17,31 @@ struct SharedData {
 
 struct SharedData* sharedData;
 
-void critical_section(int processNumber) {
-    //create message format HH:MM:SS (?)
+void critical_section(int processNumber, FILE* logFile) {
+    //create message format HH:MM:SS
     time_t t;
     time(&t);
     char message[50];
     strftime(message, sizeof(message), "%H:%M:%S", localtime(&t));
-    printf("%s File modified by process number %d\n", message, processNumber);
+    fprintf(logFile, "%s Process %d entered critical section. \n", message, processNumber);
+
+    //write message to cstest
+    FILE* cstest = fopen("cstest", "a");
+    fprintf(cstest, "%s File modified by process number %d\n", message, processNumber);
+    fclose(cstest);
 }
 
-void entry_section (int processNumber) {
-    printf("Process %d entering critical section. \n", processNumber);
+void entry_section (int processNumber, FILE* logFile) {
+    fprintf(logFile, "Process %d entering critical section. \n", processNumber);
     sharedData[processNumber - 1].inCritical = 1;
 }
 
-void exit_section (int processNumber) {
-    printf("Process %d exiting critical section. \n", processNumber);
+void exit_section (int processNumber, FILE* logFile) {
+    fprintf(logFile, "Process %d exiting critical section. \n", processNumber);
     sharedData[processNumber - 1].inCritical = 0;
 }
 
-void remainder_section() {
+void random_sleep() {
     //sleep for random amount of time between 1 and 3 seconds
     sleep(rand() % 3 + 1);
 }
@@ -44,14 +53,29 @@ int main(int argc, char* argv[]) {
     int shmid = shmget(key, sizeof(struct SharedData) * MAX_PROCESSES, 0666);
     sharedData = (struct SharedData*)shmat(shmid, NULL, 0);
 
-    for (int i = 0; i < 5; ++i) {
-        entry_section(processNumber);
-        sleep(rand() % 3 + 1);
-        critical_section(processNumber);
-        sleep(rand() % 3 + 1);
-        exit_section(processNumber);
+    //logFile logic
+    char logFileName[20];
+    snprintf(logFileName, sizeof(logFileName), "logfile.%d", processNumber);
+    FILE* logFile = fopen(logFileName, "w");
+    if (logFile == NULL) {
+        perror("SLAVE ERROR: Problem opening log file");
+        exit(EXIT_FAILURE);
     }
 
+    //loop for critical section
+    for (int i = 0; i < 5; ++i) {
+        entry_section(processNumber, logFile);
+        random_sleep();
+        critical_section(processNumber, logFile);
+        random_sleep();
+        exit_section(processNumber, logFile);
+        //remainder_section();
+    }
+
+    //close logFile
+    fclose(logFile);
+
+    //Detach from shared memory
     shmdt (sharedData);
 
     return 0;
